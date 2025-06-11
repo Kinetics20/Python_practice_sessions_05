4cf9081deb37187bfc9c85118d24b6f051548267
Kinetics20 <kinetics20@gmail.com>
2025-06-11 19:44:54 +0200

    add changes to code

--- demos/howmuchcode.py
+++ demos/howmuchcode.py
@@ -3,13 +3,42 @@ import time
 
 DIR_TO_SCAN = '/home/lipov/projects/Python_practice_sessions_05/demos/random-stuff'
 
+EXT_TO_LANG = {
+    '.c': 'C/C++',
+    '.cc': 'C/C++',
+    '.h': 'C/C++',
+    '.hpp': 'C/C++',
+    '.cpp': 'C/C++',
+    '.py': 'Python',
+    '.pyw': 'Python',
+    '.html': 'HTML',
+    '.htm': 'HTML',
+    '.css': 'CSS',
+    '.js': 'JavaScript',
+    '.bat': 'Batch',
+    '.pl': 'Perl',
+    '.php': 'PHP',
+
+}
+
+
 def get_stats(stats):
     for path, dirs, files in os.walk(DIR_TO_SCAN):
-        print(files)
+        for file in files:
+            full_fname = f'{path}/{file}'
+        _, ext = os.path.splitext(file)
+
+        ext = ext.lower()
+
+        lang = EXT_TO_LANG.get(ext)
+        if lang is None:
+            print(ext)
+            continue
 
 
 def show_stats(stats):
     pass
+
 
 def main():
     stats = {
@@ -22,9 +51,7 @@ def main():
     while True:
         get_stats(stats)
         show_stats(stats)
-        time.sleep(1.5) # Seconds.
-
-
+        time.sleep(1.5)  # Seconds.
 
 
 if __name__ == '__main__':
