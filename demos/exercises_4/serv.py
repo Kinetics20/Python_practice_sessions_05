8409b02dd23735717e4dad8b25ed8ee42e8dbabc
Kinetics20 <kinetics20@gmail.com>
2025-07-02 17:52:29 +0200

    testing socket library

---
+++ demos/exercises_4/serv.py
@@ -0,0 +1,28 @@
+import socket
+import time
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(('0.0.0.0', 1234))
+s.listen(10)
+
+c, addr = s.accept()
+print("CONNECTED:", addr)
+
+c.shutdown(socket.SHUT_RDWR)
+c.close()
+
+s.close()
+
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+#     s.bind((HOST, PORT))
+#     s.listen(1)
+#     conn, addr = s.accept()
+#     with conn:
+#         print('Connected by', addr)
+#         while True:
+#             data = conn.recv(1024)
+#             if not data:
+#                 break
+#                 conn.sendall(data)
