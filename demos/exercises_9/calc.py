f9cea428cc741dc9b11c8af2d0b00f06fbf7e558
Kinetics20 <kinetics20@gmail.com>
2025-08-27 01:57:37 +0200

    calc in tkinter

--- demos/exercises_9/calc.py
+++ demos/exercises_9/calc.py
@@ -7,15 +7,23 @@ class CalcWindow:
         root.title("Calculator")
         root.geometry("800x600")
 
-        root.columnconfigure(0, weight=1)
-        root.rowconfigure(0, weight=1)
+        # root.columnconfigure(0, weight=1)
+        # root.rowconfigure(0, weight=1)
 
-        frame = ttk.Frame(root)
-        frame.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
+        # result_frame = ttk.Frame(root)
+        # result_frame.grid(row=0, column=0, columnspan=2)
+        #
+        # entry = ttk.Entry(root)
+        # entry.grid(row=0, column=0, sticky = (tk.W, tk.E, tk.N, tk.S))
+
+        digits_frame = ttk.Frame(root)
+        digits_frame.grid(row=1, column=0)
+
+        # sticky = (tk.W, tk.E, tk.N, tk.S)
 
         buttons = []
         for i in range(0, 10):
-            button = ttk.Button(frame, text=f'{i}')
+            button = ttk.Button(digits_frame, text=f'{i}', padding=10)
             buttons.append(button)
 
         for i, x, y in [
@@ -31,11 +39,25 @@ class CalcWindow:
         ]:
             buttons[i].grid(row=y, column=x)
 
-        button = ttk.Button(frame, text="0")
+        button = ttk.Button(digits_frame, text="0", padding=10)
         button.grid(row=3, column=0, columnspan=3, sticky=(tk.W, tk.E, tk.N, tk.S))
 
 
+        ops_frame = ttk.Frame(root)
+        ops_frame.grid(row=1, column=1)
 
+        for i, op in enumerate([
+            '+', '-', '*', '/'
+        ]):
+
+            button = ttk.Button(ops_frame, text=op, padding=10)
+            button.grid(row=i, column=0)
+
+        result_frame = ttk.Frame(root)
+        result_frame.grid(row=0, column=0, columnspan=2)
+
+        entry = ttk.Entry(root, state='readonly')
+        entry.grid(row=0, column=0, sticky = (tk.W, tk.E, tk.N, tk.S))
 
 
 root = tk.Tk()
---
+++ demos/exercises_9/test_tk.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+
+root = tk.Tk()
+e = tk.Entry(root)
+e.pack()
+root.mainloop()
