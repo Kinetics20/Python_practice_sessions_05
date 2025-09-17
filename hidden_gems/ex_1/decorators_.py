b580eb9357285d7c359cbabb55501c86ba70d702
Kinetics20 <kinetics20@gmail.com>
2025-09-17 17:24:02 +0200

    triple closure with many parameters

--- hidden_gems/ex_1/decorators_.py
+++ hidden_gems/ex_1/decorators_.py
@@ -1,7 +1,9 @@
 
 def upper(fn):
-    def inner(name):
-        return fn(name.title())
+    def inner(*args):
+        args = [arg.title() for arg in args]
+        # kwargs = {k: v.title() for k, v in kwargs}
+        return fn(*args)
 
     return inner
 
@@ -12,15 +14,29 @@ def reverse(fn):
 
     return inner
 
-@reverse
+def html(tag='h1'):
+    def wrapper(fn):
+        def inner(*args):
+            args_ = list(args)
+            args_[0] = '<b>' + args[0]
+            args_[-1] = args_[-1] + '</b>'
+
+            html_text = f'<{tag}>{fn(*args_)}</{tag}>'
+            return html_text
+
+        return inner
+    return wrapper
+
 @upper
-def hello(name):
+@html()
+def bye(name, last_name, /):
+    return f'Bye, {name} {last_name}!'
+
+
+@upper
+@html(tag='h2')
+def hello(name, /):
     return f'Hello, {name}!'
 
-@upper
-@reverse
-def bye(name):
-    return f'Bye, {name}!'
-
+print(bye('John', 'Smith'))
 print(hello('John'))
-print(bye('John'))
