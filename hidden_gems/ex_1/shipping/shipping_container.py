265eaee5c216a4b5c1277fac398ca220889a3959
Kinetics20 <kinetics20@gmail.com>
2025-09-19 17:56:24 +0200

    intro to oop

---
+++ hidden_gems/ex_1/shipping/shipping_container.py
@@ -0,0 +1,12 @@
+from debugpy.common.singleton import Singleton
+
+
+class ShippingContainer(object, metaclass=type):
+
+    next_serial = 1337
+
+    def __init__(self, owner_code, contents):
+        self.owner_code = owner_code
+        self.contents = contents
+        self.serial = ShippingContainer.next_serial
+        ShippingContainer.next_serial += 1
