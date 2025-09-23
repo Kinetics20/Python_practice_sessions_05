d6b7a5f3fca386995431d567c15c6b48f9aff4f9
Kinetics20 <kinetics20@gmail.com>
2025-09-23 17:57:22 +0200

    basic class for iterable and next method

---
+++ hidden_gems/ex_1/ex_4.py
@@ -0,0 +1,17 @@
+class Sth:
+    def __init__(self):
+        self.value = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.value > 5:
+            raise StopIteration
+
+        result = self.value
+        self.value += 1
+        return result
+
+for element in Sth():
+    print(element)
