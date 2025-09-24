6d2c0555bbd1aa8dad891e71235b900b0b17d827
Kinetics20 <kinetics20@gmail.com>
2025-09-24 21:57:23 +0200

    added new inherriting class

--- hidden_gems/ex_1/shipping/shipping_container.py
+++ hidden_gems/ex_1/shipping/shipping_container.py
@@ -1,3 +1,6 @@
+from hidden_gems.ex_1.shipping import iso6346
+
+
 class ShippingContainer(object, metaclass=type):
     next_serial = 1337
 
@@ -7,18 +10,55 @@ class ShippingContainer(object, metaclass=type):
         cls.next_serial += 1
         return result
 
+    @staticmethod
+    def _make_bic_code(owner_code, serial):
+        return iso6346.create(
+            owner_code=owner_code,
+            serial=str(serial).zfill(6),
+        )
+
 
     @classmethod
-    def create_empty(cls, owner_code):
-        return cls(owner_code, contents=[])
+    def create_empty(cls, owner_code, **kwargs):
+        return cls(owner_code, contents=[], **kwargs)
 
 
     @classmethod
-    def create_empty_items(cls, owner_code, items):
-        return cls(owner_code, contents=list(items))
+    def create_with_items(cls, owner_code, items, **kwargs):
+        return cls(owner_code, contents=list(items), **kwargs)
 
 
-    def __init__(self, owner_code, contents):
+    def __init__(self, owner_code, contents, **kwargs):
         self.owner_code = owner_code
         self.contents = contents
-        self.serial = ShippingContainer._generate_serial()
+        self.bic = self._make_bic_code(
+            owner_code=owner_code,
+            serial=ShippingContainer._generate_serial()
+        )
+
+
+class RefrigeratedShippingContainer(ShippingContainer):
+
+    MAX_CELSIUS = 4.0
+
+    def __init__(self, owner_code, contents, *, celsius, **kwargs):
+        super().__init__(owner_code, contents, **kwargs)
+        if celsius > RefrigeratedShippingContainer.MAX_CELSIUS:
+            raise ValueError('Temperature is too high')
+        self.celsius = celsius
+
+    @staticmethod
+    def _make_bic_code(owner_code, serial):
+        return iso6346.create(
+            owner_code=owner_code,
+            serial=str(serial).zfill(6),
+            category='R'
+        )
+
+
+# r1 = RefrigeratedShippingContainer('MAE', ['penguin'])
+# print(r1.bic)
+
+r0 = RefrigeratedShippingContainer('YML', ['onion'], celsius=3.0)
+r1 = RefrigeratedShippingContainer.create_empty('YML', celsius=3.0)
+r2 = RefrigeratedShippingContainer.create_with_items('YML', ['ice', 'fishes'],  celsius=3.0)
