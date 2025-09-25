dd7a78f12bcdbc8af97bedbf4584f5b9932fbed2
Kinetics20 <kinetics20@gmail.com>
2025-09-25 17:56:25 +0200

    add getter and setter for celsius and fahrenheit

--- hidden_gems/ex_1/shipping/shipping_container.py
+++ hidden_gems/ex_1/shipping/shipping_container.py
@@ -17,16 +17,13 @@ class ShippingContainer(object, metaclass=type):
             serial=str(serial).zfill(6),
         )
 
-
     @classmethod
     def create_empty(cls, owner_code, **kwargs):
         return cls(owner_code, contents=[], **kwargs)
 
-
     @classmethod
     def create_with_items(cls, owner_code, items, **kwargs):
         return cls(owner_code, contents=list(items), **kwargs)
-
 
     def __init__(self, owner_code, contents, **kwargs):
         self.owner_code = owner_code
@@ -38,14 +35,31 @@ class ShippingContainer(object, metaclass=type):
 
 
 class RefrigeratedShippingContainer(ShippingContainer):
-
     MAX_CELSIUS = 4.0
 
     def __init__(self, owner_code, contents, *, celsius, **kwargs):
         super().__init__(owner_code, contents, **kwargs)
-        if celsius > RefrigeratedShippingContainer.MAX_CELSIUS:
-            raise ValueError('Temperature is too high')
         self.celsius = celsius
+
+    @property
+    def celsius(self):
+        return f'{self._celsius}°C'
+
+    @celsius.setter
+    def celsius(self, value):
+        if not (-273.16 < value < RefrigeratedShippingContainer.MAX_CELSIUS):
+            raise ValueError('Invalid Temperature!')
+        self._celsius = value
+
+
+    @property
+    def fahrenheit(self):
+        return self._celsius * 9 / 5 + 32
+
+    @fahrenheit.setter
+    def fahrenheit(self, value):
+        self.celsius = (value - 32) * 5 / 9
+
 
     @staticmethod
     def _make_bic_code(owner_code, serial):
@@ -61,4 +75,6 @@ class RefrigeratedShippingContainer(ShippingContainer):
 
 r0 = RefrigeratedShippingContainer('YML', ['onion'], celsius=3.0)
 r1 = RefrigeratedShippingContainer.create_empty('YML', celsius=3.0)
-r2 = RefrigeratedShippingContainer.create_with_items('YML', ['ice', 'fishes'],  celsius=3.0)
+r2 = RefrigeratedShippingContainer.create_with_items('YML', ['ice', 'fishes'], celsius=3.0)
+r0.celsius = 2
+print(r0.celsius)
