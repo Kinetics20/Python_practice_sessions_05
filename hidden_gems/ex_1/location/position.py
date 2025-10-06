57999db95d55846e7cae55e31d1da620df32190d
Kinetics20 <kinetics20@gmail.com>
2025-10-06 17:55:30 +0200

    class decorators, repr, str, format

---
+++ hidden_gems/ex_1/location/position.py
@@ -0,0 +1,66 @@
+class Position:
+    def __init__(self, latitude, longitude):
+        self._latitude = latitude
+        self._longitude = longitude
+
+    @property
+    def latitude_hemisphere(self):
+        return 'N' if self._latitude >= 0 else 'S'
+
+    @property
+    def longitude_hemisphere(self):
+        return 'E' if self._longitude >= 0 else 'W'
+
+
+    def __repr__(self):
+        return f'{type(self).__name__}(latitude={self._latitude}, longitude={self._longitude})'
+
+    def __str__(self):
+        return format(self)
+
+    def __format__(self, format_spec):
+        component_format_spec = '.2f'
+
+        prefix, dot, suffix = format_spec.partition('.')
+
+        if dot:
+            num_decimal_places = int(suffix)
+            component_format_spec = f'.{num_decimal_places}f'
+
+        latitude = format(abs(self._latitude), component_format_spec)
+        longitude = format(abs(self._longitude), component_format_spec)
+
+        return f'{latitude}°{self.latitude_hemisphere}, {longitude}°{self.longitude_hemisphere}'
+
+p = Position(latitude=39.78, longitude=-122.42)
+print(p)
+# print(dir(object))
+
+class EarthPosition(Position):
+    pass
+
+class Location:
+    def __init__(self, name, position):
+        self._name = name
+        self._position = position
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def position(self):
+        return self._position
+
+    def __repr__(self):
+        return f'{type(self).__name__}(name={self.name!r}, position={self.position!r})'
+
+    def __str__(self):
+        return self.name
+
+
+hong_kong = Location('Hong Kong', EarthPosition(22.29, 114.16))
+cape_town = Location('Cape Town', EarthPosition(-33.93, 18.42))
+
+print(f'{hong_kong!r}')
+print(f'{cape_town!r}')
