19f35ec6687034d997a0cf1a309f722f5061ca61
Kinetics20 <kinetics20@gmail.com>
2025-10-07 17:55:35 +0200

    class decorator auto-repr

--- hidden_gems/ex_1/location/position.py
+++ hidden_gems/ex_1/location/position.py
@@ -1,7 +1,45 @@
+import inspect
+
+
+def auto_repr(cls):
+    members = vars(cls)
+
+    if '__repr__' in members:
+        raise TypeError(f'{cls.__name__} already has a __repr__ method')
+
+    if '__init__' not in members:
+        raise TypeError(f'{cls.__name__} does not override __init__')
+
+    signature = inspect.signature(cls.__init__)
+    parameter_names = list(signature.parameters)[1:]
+
+    if not all(members.get(name, None) for name in parameter_names):
+        raise TypeError(f'Cannot apply auto_repr to {cls.__name__} because not all'
+                        f'__init__ parameters have matching properties.')
+
+    def synthesized_repr(self):
+        return '{type_class}({args})'.format(
+            type_class=type(self).__name__,
+            args=', '.join(f'{name}={getattr(self, name)!r}' for name in parameter_names)
+        )
+
+    setattr(cls, '__repr__', synthesized_repr)
+
+    return cls
+
+@auto_repr
 class Position:
     def __init__(self, latitude, longitude):
         self._latitude = latitude
         self._longitude = longitude
+
+    @property
+    def latitude(self):
+        return self._latitude
+
+    @property
+    def longitude(self):
+        return self._longitude
 
     @property
     def latitude_hemisphere(self):
@@ -11,9 +49,8 @@ class Position:
     def longitude_hemisphere(self):
         return 'E' if self._longitude >= 0 else 'W'
 
-
-    def __repr__(self):
-        return f'{type(self).__name__}(latitude={self._latitude}, longitude={self._longitude})'
+    # def __repr__(self):
+    #     return f'{type(self).__name__}(latitude={self._latitude}, longitude={self._longitude})'
 
     def __str__(self):
         return format(self)
@@ -32,13 +69,18 @@ class Position:
 
         return f'{latitude}°{self.latitude_hemisphere}, {longitude}°{self.longitude_hemisphere}'
 
+
 p = Position(latitude=39.78, longitude=-122.42)
 print(p)
+
+
 # print(dir(object))
 
 class EarthPosition(Position):
     pass
 
+
+@auto_repr
 class Location:
     def __init__(self, name, position):
         self._name = name
@@ -52,8 +94,8 @@ class Location:
     def position(self):
         return self._position
 
-    def __repr__(self):
-        return f'{type(self).__name__}(name={self.name!r}, position={self.position!r})'
+    # def __repr__(self):
+    #     return f'{type(self).__name__}(name={self.name!r}, position={self.position!r})'
 
     def __str__(self):
         return self.name
@@ -62,5 +104,8 @@ class Location:
 hong_kong = Location('Hong Kong', EarthPosition(22.29, 114.16))
 cape_town = Location('Cape Town', EarthPosition(-33.93, 18.42))
 
+ep = EarthPosition(22.29, 114.16)
+
 print(f'{hong_kong!r}')
 print(f'{cape_town!r}')
+print(f'{ep!r}')
