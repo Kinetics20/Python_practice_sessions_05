8266928ce670385a8357be4921825fbb4afe3ceb
Kinetics20 <kinetics20@gmail.com>
2025-10-20 17:59:47 +0200

    add InOrderIterator, SkipMissingIterator

--- hidden_gems/ex_1/iterattions/iterators_ultimate.py
+++ hidden_gems/ex_1/iterattions/iterators_ultimate.py
@@ -77,3 +77,82 @@ iterator2 = PreOrderIterator(expr_tree)
 # print(' '.join(iterator2))
 
 # In-order Traversal, Depth-first
+
+
+class InOrderIterator:
+    def __init__(self, sequence):
+        if not is_perfect_length(sequence):
+            raise ValueError('Not a perfect binary tree with lengths 2ⁿ - 1.')
+
+        self._sequence = sequence
+        self._stack = []
+        self._index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if (self._index >= len(self._sequence)) and (len(self._stack) == 0):
+            raise StopIteration
+
+        while self._index < len(self._sequence):
+            self._stack.append(self._index)
+            self._index = left_child(self._index)
+
+        index = self._stack.pop()
+        result = self._sequence[index]
+        self._index = right_child(index)
+
+        return result
+
+iterator3 = InOrderIterator(expr_tree)
+# print(' '.join(iterator3))
+
+
+class SkipMissingIterator:
+    def __init__(self, iterable):
+        self._iterator = iter(iterable)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while True:
+            item = next(self._iterator)
+            if item is not missing:
+                return item
+
+
+missing = object()
+
+# r + p * q
+expr_tree = ['*', 'r', '*', missing, missing, 'p', 'q']
+
+iterator4 = SkipMissingIterator(InOrderIterator(expr_tree))
+# print(' '.join(iterator4))
+
+
+typesetting_table = {
+    '-': '\u2212',
+    '*': '\u00D7',
+    '/': '\u00F7'
+}
+
+
+class TranslationIterator:
+    def __init__(self, table, iterable):
+        self._table = table
+        self._iterator = iter(iterable)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        item = next(self._iterator)
+        return self._table.get(item, item)
+
+# r + p * q
+expr_tree = ['*', 'r', '/', missing, missing, 'p', 'q']
+
+iterator5 = TranslationIterator(typesetting_table, SkipMissingIterator(InOrderIterator(expr_tree)))
+print(' '.join(iterator5))
