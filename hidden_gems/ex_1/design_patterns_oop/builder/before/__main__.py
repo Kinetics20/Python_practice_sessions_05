8959e75497fd87608dae9eb345eef3cde15f9d50
Kinetics20 <kinetics20@gmail.com>
2025-10-23 16:47:27 +0200

    prototype for builder

---
+++ hidden_gems/ex_1/design_patterns_oop/builder/before/__main__.py
@@ -0,0 +1,12 @@
+from computer import Computer
+
+computer = Computer(
+    case='Coolermaster',
+    mainboard='MSI',
+    cpu='Intel Core i9',
+    memory='2 X 16GB',
+    hard_drive='SSD 2TB',
+    video_card='GeForce RTX4080'
+)
+
+computer.display()
