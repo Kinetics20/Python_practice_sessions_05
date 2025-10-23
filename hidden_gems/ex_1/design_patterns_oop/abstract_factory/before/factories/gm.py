a6a3f1e9884494278ce39dbd2b1173ff7ef18b1e
Kinetics20 <kinetics20@gmail.com>
2025-10-23 14:05:52 +0200

    basic concept for complex choices cars in the same class

---
+++ hidden_gems/ex_1/design_patterns_oop/abstract_factory/before/factories/gm.py
@@ -0,0 +1,17 @@
+class ChevySpark:
+    def start(self):
+        print('Chevy spark running efficiently')
+    def stop(self):
+        print('Chevy spark shutting down')
+
+class ChevyCamaro:
+    def start(self):
+        print('Chevy Camaro V8 sounding awesome!')
+    def stop(self):
+        print('Chevy Camaro shutting down')
+
+class CadillacCTS:
+    def start(self):
+        print('Cadillac CTS purring luxuriously')
+    def stop(self):
+        print('Cadillac CTS shutting down')
