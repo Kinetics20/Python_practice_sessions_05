20f2d9c28491d4d1a1cad035882477fe0b7aad33
Kinetics20 <kinetics20@gmail.com>
2025-10-23 12:45:10 +0200

    simple factor

--- hidden_gems/ex_1/design_patterns_oop/factory/after/autofactory.py
+++ hidden_gems/ex_1/design_patterns_oop/factory/after/autofactory.py
@@ -2,6 +2,7 @@ from inspect import getmembers, isclass, isabstract
 
 import autos
 
+
 # from hidden_gems.ex_1.design_patterns_oop.factory.after import autos
 
 
@@ -14,12 +15,17 @@ class AutoFactory:
     def load_autos(self):
         classes = getmembers(autos, lambda m: isclass(m) and not isabstract(m))
 
-        # print(classes)
         for name, _cls in classes:
             if issubclass(_cls, autos.AbsAuto):
                 type(self).autos[name] = _cls
 
+    def create_instance(self, car_name):
+        if car_name in type(self).autos:
+            return self.autos[car_name]()
 
-if __name__ == '__main__':
-    af = AutoFactory()
-    print(af.autos)
+        return autos.NullCar(car_name)
+
+
+# if __name__ == '__main__':
+#     af = AutoFactory()
+#     print(af.autos)
