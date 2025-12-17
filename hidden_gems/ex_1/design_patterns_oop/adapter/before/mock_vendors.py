73d9f640c5dd1048d4b9b45076b8d6d4c5fbc0e6
Kinetics20 <kinetics20@gmail.com>
2025-12-17 17:53:23 +0100

    design patter -> class adapter

---
+++ hidden_gems/ex_1/design_patterns_oop/adapter/before/mock_vendors.py
@@ -0,0 +1,7 @@
+from vendor import Vendor
+
+MOCKCVENDORS = (
+    Vendor('Pizza Love', '33', 'Pepperoni Lane'),
+    Vendor('Happy and Green', '25', 'Kale St.'),
+    Vendor('Sweet Tooth', '42', 'Chocolate Ave.')
+)
