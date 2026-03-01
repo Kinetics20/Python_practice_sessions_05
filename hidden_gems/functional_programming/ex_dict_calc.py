841a513578c0a4aba6d713391b6543faf605ab8a
Kinetics20 <kinetics20@gmail.com>
2026-03-01 20:49:58 +0100

    dict, lambda, get

--- hidden_gems/functional_programming/ex_dict_calc.py
+++ hidden_gems/functional_programming/ex_dict_calc.py
@@ -3,13 +3,29 @@ def dict_calc(a, b, operation):
         'add': lambda: a + b,
         'multiply': lambda: a * b,
         'substraction': lambda: a - b,
-        'divide': lambda: a / b
+        'divide': lambda: None if b == 0 else a / b
     }
 
-    return dict_opr[operation]()
+    return None if operation not in dict_opr else dict_opr[operation]()
+
+
+def calculate(a, operation, b):
+    if operation == "/" and b == 0:
+        return None
+
+    dict_operation = {
+        '+': lambda: a + b,
+        '-': lambda: a - b,
+        '*': lambda: a * b,
+        '/': lambda: a / b,
+    }
+
+    return dict_operation.get(operation, lambda: None)()
 
 
 print(dict_calc(2, 5, 'add'))
 print(dict_calc(2, 5, 'multiply'))
 print(dict_calc(2, 5, 'substraction'))
 print(dict_calc(2, 5, 'divide'))
+print(dict_calc(2, 5, 'ala'))
+print(dict_calc(2, 5, 0))
