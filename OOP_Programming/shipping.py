8f033d4a6728be82c3a97d768b18914858800d98
Kinetics20 <kinetics20@gmail.com>
2026-04-09 17:14:08 +0200

    oop, getter, setter, inheritance, super

--- OOP_Programming/shipping.py
+++ OOP_Programming/shipping.py
@@ -2,6 +2,9 @@ import bic_code
 
 
 class ShippingContainer:
+    HEIGHT_FT = 8.5
+    WIDTH_FT = 8.0
+
     next_serial = 1337
 
     @staticmethod
@@ -17,31 +20,36 @@ class ShippingContainer:
             serial=str(serial).zfill(6),
         )
 
-    def __init__(self, owner_code, contents, **kwargs):
+    def __init__(self, owner_code, length_ft, contents, **kwargs):
         self.owner_code = owner_code
+        self.length_ft = length_ft
         self.contents = contents
         self.bic = self._make_bic_code(
             owner_code=owner_code,
             serial=type(self)._generate_serial()
         )
 
-    @classmethod
-    def create_empty(cls, owner_code, **kwargs):
-        return cls(owner_code, contents=[], **kwargs)
+    @property
+    def volume_ft3(self):
+        return type(self).HEIGHT_FT * type(self).WIDTH_FT * self.length_ft
 
     @classmethod
-    def create_with_items(cls, owner_code, items, **kwargs):
-        return cls(owner_code, contents=list(items), **kwargs)
+    def create_empty(cls, owner_code, length_ft, **kwargs):
+        return cls(owner_code, length_ft, contents=[], **kwargs)
+
+    @classmethod
+    def create_with_items(cls, owner_code, length_ft, items, **kwargs):
+        return cls(owner_code, length_ft, contents=list(items), **kwargs)
 
 
 class RefrigeratedShippingContainer(ShippingContainer):
     MAX_CELSIUS = 4.0
+    FRIDGE_VOLUME_FT3 = 100
 
-    def __init__(self, owner_code, contents, *, celsius, **kwargs):
-        super().__init__(owner_code, contents, **kwargs)
-        if celsius > type(self).MAX_CELSIUS:
-            raise ValueError(f'Temperature too hot')
-        self._celsius = celsius
+    def __init__(self, owner_code, length_ft, contents, *, celsius, **kwargs):
+        super().__init__(owner_code, length_ft, contents, **kwargs)
+
+        self.celsius = celsius
 
     @staticmethod
     def _make_bic_code(owner_code, serial):
@@ -52,8 +60,37 @@ class RefrigeratedShippingContainer(ShippingContainer):
         )
 
     @property
+    def volume_ft3(self):
+        return super().volume_ft3 - type(self).FRIDGE_VOLUME_FT3
+
+    @property
     def celsius(self):
-        return self._celsius
+        return f'{self._celsius}°C'
+
+    @celsius.setter
+    def celsius(self, value):
+        if value > type(self).MAX_CELSIUS:
+            raise ValueError(f'Temperature too hot')
+        self._celsius = value
+
+    @property
+    def temp_f(self):
+        return f'{(self._celsius * 9 / 5 + 32):.1f}°F'
+
+    @temp_f.setter
+    def temp_f(self, value):
+        self.celsius = (value - 32) * 5 / 9
+
+    @property
+    def temp_k(self):
+        return f'{(self._celsius + 273.15):.1f}°K'
+
+    @temp_k.setter
+    def temp_k(self, value):
+        self.celsius = value - 273.15
+
+    def __str__(self):
+        return f'Owner_code: {self.owner_code}, Bic: {self.bic}, Contents: {self.contents}, Temperature C: {self.celsius}, Temperature F: {self.temp_f}, Temperature K: {self.temp_k}'
 
 
 # s1 = ShippingContainer('MAE', ['apple', 'banana'])
@@ -67,7 +104,9 @@ class RefrigeratedShippingContainer(ShippingContainer):
 # print(ShippingContainer.next_serial)
 # print(s3.bic)
 
-r1 = RefrigeratedShippingContainer.create_with_items('MAE', ['fishes'], celsius=2)
+r1 = RefrigeratedShippingContainer.create_with_items('MAE', 20, ['fishes'], celsius=2)
 print(r1.bic)
 print(r1.celsius)
 # print(r1._make_bic_code('MAE', 1337))
+print(r1)
+print(r1.volume_ft3)
