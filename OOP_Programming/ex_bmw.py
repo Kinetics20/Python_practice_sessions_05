5f16618d40c8a90a041765035a1195065e0a0133
Kinetics20 <kinetics20@gmail.com>
2026-04-23 17:51:57 +0200

    BMW from diagram classes

---
+++ OOP_Programming/ex_bmw.py
@@ -0,0 +1,89 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from typing import override
+
+
+class Engine:
+    def __init__(self, power: int, fuel_type: str) -> None:
+        self.power: int = power
+        self._fuel_type: str = fuel_type
+
+    @property
+    def power(self) -> int:
+        return self._power
+
+    @power.setter
+    def power(self, value: int) -> None:
+        if value <= 60:
+            raise ValueError('Engine power must be greater than 60 kW.')
+        self._power = value
+
+    @property
+    def fuel_type(self) -> str:
+        return self._fuel_type
+
+    def start(self) -> str:
+        return 'Engine started'
+
+
+class ElectricEngine(Engine):
+    def __init__(self, power: int, battery_level: int) -> None:
+        super().__init__(power, fuel_type='electric')
+        self.battery_level = battery_level
+
+    @property
+    def battery_level(self) -> int:
+        return self._battery_level
+
+    @battery_level.setter
+    def battery_level(self, value: int) -> None:
+        if value < 0 or value > 100:
+            raise ValueError('Battery should not be less than zero and grater than 100.')
+        self._battery_level = value
+
+    @override
+    def start(self) -> str:
+        if self.battery_level <= 15:
+            raise ValueError('Battery level should be greater than 15 percent.')
+        return 'Electric engine started.'
+
+
+class Vehicle(ABC):
+    def __init__(self, brand: str, model: str, year: int, mileage: int) -> None:
+        self._brand: str = brand
+        self._model: str = model
+        self._year: int = year
+        self.mileage: int = mileage
+
+    @property
+    def brand(self) -> str:
+        return self._brand
+
+    @property
+    def model(self) -> str:
+        return self._model
+
+    @property
+    def year(self) -> int:
+        return self._year
+
+    @property
+    def mileage(self) -> int:
+        return self._mileage
+
+    @mileage.setter
+    def mileage(self, value: int) -> None:
+        if value < 0:
+            raise ValueError('Mileage cannot be negative.')
+        self._mileage = value
+
+    @abstractmethod
+    def start_engine(self) -> str:
+        ...
+
+    def stop_engine(self) -> str:
+        return 'Engine stopped'
+
+    def vehicle_info(self) -> str:
+        return f'Brand: {self.brand}, model: {self.model}, year: {self.year}, mileage: {self.mileage}'
