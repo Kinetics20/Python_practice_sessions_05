68fa69e8b9f787d4ff2cdc9a8120d5a1efd90a47
Kinetics20 <kinetics20@gmail.com>
2026-04-29 17:55:06 +0200

    implement employees from mermaid class diagram

---
+++ OOP_Programming/employees_it.py
@@ -0,0 +1,152 @@
+from abc import ABC, abstractmethod
+
+
+class Department:
+    def __init__(self, name: str, location: str) -> None:
+        self.name: str = name
+        self.location: str = location
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @name.setter
+    def name(self, value: str) -> None:
+        cleared = value.strip()
+        if len(cleared) < 2:
+            raise ValueError('Name must have at least 2 characters.')
+        self._name = cleared
+
+    @property
+    def location(self) -> str:
+        return self._location
+
+    @location.setter
+    def location(self, value: str) -> None:
+        cleared = value.strip()
+        if len(cleared) < 4:
+            raise ValueError('Location name must have at least 4 characters.')
+        self._location = cleared
+
+    def department_info(self) -> str:
+        return f'Department: {self.name}, {self.location}'
+
+
+class Project:
+    def __init__(self, name: str, budget: float) -> None:
+        self.name: str = name
+        self.budget: float = budget
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @name.setter
+    def name(self, value: str) -> None:
+        cleared = value.strip()
+        if len(cleared) < 2:
+            raise ValueError('Name must have at least 2 characters.')
+        self._name = cleared
+
+    @property
+    def budget(self) -> float:
+        return self._budget
+
+    @budget.setter
+    def budget(self, value: float) -> None:
+        if value < 10_000:
+            raise ValueError('Budget must be at least 10,000.')
+        self._budget = value
+
+    def project_info(self) -> str:
+        return f'Project: {self.name}, {self.budget}'
+
+
+class Employee(ABC):
+    def __init__(
+            self,
+            first_name: str,
+            last_name: str,
+            employee_id: str,
+            salary: float
+    ) -> None:
+        self._first_name: str = first_name
+        self._last_name: str = last_name
+        self._employee_id: str = employee_id
+        self.salary: float = salary
+
+    @property
+    def first_name(self) -> str:
+        return self._first_name
+
+    @property
+    def last_name(self) -> str:
+        return self._last_name
+
+    @property
+    def employee_id(self) -> str:
+        return self._employee_id
+
+    @property
+    def salary(self) -> float:
+        return self._salary
+
+    @salary.setter
+    def salary(self, value: float) -> None:
+        if value < 1000:
+            raise ValueError('Salary should be at least 1000.')
+        self._salary = value
+
+    @abstractmethod
+    def work(self) -> str:
+        ...
+
+    def get_full_name(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+
+    def employee_info(self) -> str:
+        return f'Employee info: {self.get_full_name()}, {self.employee_id}, {self.salary}'
+
+
+class ITEmployee(Employee):
+    def __init__(
+            self,
+            first_name: str,
+            last_name: str,
+            employee_id: str,
+            salary: float,
+            department: Department,
+            project: Project,
+            programming_language: str
+
+    ) -> None:
+        super().__init__(first_name, last_name, employee_id, salary)
+        self._department: Department = department
+        self._project: Project = project
+        self.programming_language: str = programming_language
+
+    @property
+    def department(self) -> Department:
+        return self._department
+
+    @property
+    def project(self) -> Project:
+        return self._project
+
+    @property
+    def programming_language(self) -> str:
+        return self._programming_language
+
+    @programming_language.setter
+    def programming_language(self, value: str) -> None:
+        cleared = value.strip()
+        if len(cleared) < 2:
+            raise ValueError('Programming language should contain 2 characters at least.')
+        self._programming_language = cleared
+
+    def assign_project(self, project: Project) -> None:
+        self._project = project
+
+    @abstractmethod
+    def write_code(self) -> str:
+        ...
